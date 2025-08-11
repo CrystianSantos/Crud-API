f5ddb67437a2e498d0bb3faa6df8c25b43be2e0e
Crystian <crystiansantos6639@gmail.com>
2025-08-11 20:57:05 -0300

    NOME ALTERADI

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://crud-api-rfl7.onrender.com/api';
+const API_BASE_URL = 'https://crud-api-rfl7.onrender.com/api';
 
 // Elementos DOM
 const form = document.getElementById('form-contato');
